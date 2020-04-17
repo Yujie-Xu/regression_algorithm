0bf5da581ce0b352517a2762ad823caf7e378e8d
Yujie <yujiexu89@gmail.com>
2020-04-16 23:43:25 -0400

    first commit

---
+++ src/data/cmake-build-debug/CMakeFiles/data.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data.dir/make_dataset.cpp.o"
+  "data"
+  "data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
